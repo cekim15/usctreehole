25b6e2484f0c38688e43a3782579f7a2454d7817
Christina Kim <cekim@usc.edu>
2024-11-01 16:36:07 -0700

    added post class

---
+++ app/src/main/java/Post.java
@@ -0,0 +1,25 @@
+public class Post {
+    private String title;
+    private String content;
+    private long timestamp;
+
+    public Post() {}
+
+    public Post(String title, String content, long timestamp) {
+        this.title = title;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
